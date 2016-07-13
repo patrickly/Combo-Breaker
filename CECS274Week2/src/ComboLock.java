04d97b1a8c65c586736686fbff767683b5fb54a0
admin <admin@DESKTOP-H9A2BH3>
2016-07-12 21:27:49 -0700

    Constructor and accessor added

--- CECS274Week2/src/ComboLock.java
+++ CECS274Week2/src/ComboLock.java
@@ -8,5 +8,28 @@ public class ComboLock {
 	private int mCombo3;
 	private int mDial;
 
+	/**
+	 *Constructor 
+	 *
+	 * @param combo1 Enter integer values for the range: 0-39
+	 * @param combo2 Enter integer values for the range: 0-39
+	 * @param combo3 Enter integer values for the range: 0-39
+	 */
+	public ComboLock(int combo1, int combo2,
+	 int combo3) {
+	   
+		mCombo1 = combo1;
+		mCombo2 = combo2;
+		mCombo3 = combo3;
+		mDial = 0;
 	
+	}
+	
+	/**
+	 * 
+	 * @return mDial Returns the current dial position of the lock
+	 */
+	public int getDialPosition(){
+	   return mDial; 
+	}
 }
