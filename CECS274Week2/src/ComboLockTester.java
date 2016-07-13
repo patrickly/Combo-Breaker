111eb1461deca84d62a3c309380101e732cb5011
patrickly <patrickly@dhcp-39-54-216.dhcpint.csulb.edu>
2016-07-13 11:27:03 -0700

    Added comments on a hint provided by Mr. T

--- CECS274Week2/src/ComboLockTester.java
+++ CECS274Week2/src/ComboLockTester.java
@@ -20,6 +20,10 @@ after ticks decrement -5
 after past 0 mark 35
 35
 
+ticks can be above 39, such as 100, 200.
+ so a math operator to deal with that is using modulus. 
+ thanks to Mr. T for the hint
+
 */
 
 //testing
