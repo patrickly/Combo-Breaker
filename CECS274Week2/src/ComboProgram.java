227fd45c95b50adf21846c8716e8fd413979d95c
patrickly <patrickly@dhcp-39-54-216.dhcpint.csulb.edu>
2016-07-14 11:22:33 -0700

    Brainstorming on how to get ticks values. Comments are my notes.

--- CECS274Week2/src/ComboProgram.java
+++ CECS274Week2/src/ComboProgram.java
@@ -12,13 +12,13 @@ public static void main(String[] args) {
    int rng1 = generator.nextInt(40);
    int rng2 = generator.nextInt(40);
    int rng3 = generator.nextInt(40);
-   int ex = 10;
+   int ex = 10, ex2 = 13, ex3 = 27;
    ComboLock randomLock = new ComboLock(ex, rng2, rng3);
    
-   int getDial1 = randomLock.getDialPosition();
+   int getDialinit = randomLock.getDialPosition();
  //  int getDial3 = randomLock.getDialPosition();
 
-  System.out.println("get initial dial position " + getDial1 + "\n\n----");
+  System.out.println("get initial dial position " + getDialinit + "\n\n----");
  // int ticks1 = 30; // how to get 30 when ex is 10? 40 +(-10) = 30.    fullRotation is 40. 
   // fullRotation + ex = 40 + 10 = 50. 
   // fullRotation + ex(-1) = 40 - 10 = 30.
@@ -28,11 +28,24 @@ public static void main(String[] args) {
   
   randomLock.turnRight(ticks1);
   
-  int getDial2 = randomLock.getDialPosition();
+  int getDial1 = randomLock.getDialPosition();
 
-  System.out.println("\n\n----\n get 1st dial position " + getDial2);
-  // randomLock.turnLeft();
+  System.out.println("\n\n----\n get 1st dial position " + getDial1);
+  
+ // int ticks2 = 3;    // so we want to get 3, when ex is 10, and ex2 is 13. well. 13-10 = 3
+  // ex2 - ex = 3.
+  int ticks2 = ex2 - ex;
+  
+  randomLock.turnLeft(ticks2);
+  System.out.println("\n\n----\n get 2nd dial position " + randomLock.getDialPosition());
 
+  
+  int ticks3 = 26;  // goal is to get ticks3 to equal 26, when ex is 10, ex2 is 13, and when ex3 is 27.
+  // 13+26 = 39.    27-26 = 1.    13-(13) = 0.   0-(10) = 30.   30-(3) = 27.  (13)+(10)+(3) = 26. added these parentheses and got 26.  
+  randomLock.turnRight(ticks3);
+  System.out.println("\n\n----\n get 3rd dial position " + randomLock.getDialPosition());
+
+  
   /*
    System.out.println("(Random secret combination: " + rng1 + 
     " " + rng2 + " " + rng3 + ")");
@@ -41,7 +54,7 @@ public static void main(String[] args) {
   
   
   System.out.println("(1st combo testing secret combination: " + ex + 
-   " " + rng2 + " " + rng3 + ")");
+   " " + ex2 + " " + ex3 + ")");
 
   
 
