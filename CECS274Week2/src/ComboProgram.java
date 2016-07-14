ad9bcce08e325a9999a63dddb707fe7bd41a4df5
patrickly <patrickly@dhcp-39-54-216.dhcpint.csulb.edu>
2016-07-14 11:03:37 -0700

    Trying to calculate a ticks value

--- CECS274Week2/src/ComboProgram.java
+++ CECS274Week2/src/ComboProgram.java
@@ -12,12 +12,37 @@ public static void main(String[] args) {
    int rng1 = generator.nextInt(40);
    int rng2 = generator.nextInt(40);
    int rng3 = generator.nextInt(40);
-   ComboLock randomLock = new ComboLock(rng1, rng2, rng3);
+   int ex = 10;
+   ComboLock randomLock = new ComboLock(ex, rng2, rng3);
+   
+   int getDial1 = randomLock.getDialPosition();
+ //  int getDial3 = randomLock.getDialPosition();
 
+  System.out.println("get initial dial position " + getDial1 + "\n\n----");
+ // int ticks1 = 30; // how to get 30 when ex is 10? 40 +(-10) = 30.    fullRotation is 40. 
+  // fullRotation + ex = 40 + 10 = 50. 
+  // fullRotation + ex(-1) = 40 - 10 = 30.
+  // So to ticks1 = fullRotation + (-1)ex.
+  int fullRotation = 40;
+  int ticks1 = fullRotation - ex; 
+  
+  randomLock.turnRight(ticks1);
+  
+  int getDial2 = randomLock.getDialPosition();
+
+  System.out.println("\n\n----\n get 1st dial position " + getDial2);
+  // randomLock.turnLeft();
+
+  /*
    System.out.println("(Random secret combination: " + rng1 + 
     " " + rng2 + " " + rng3 + ")");
  
-   
+   */
+  
+  
+  System.out.println("(1st combo testing secret combination: " + ex + 
+   " " + rng2 + " " + rng3 + ")");
+
   
 
   }
