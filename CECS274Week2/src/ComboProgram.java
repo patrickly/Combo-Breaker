766f5b5322126d040fa37d1870f8d1c9bab1ce7a
patrickly <patrickly@dhcp-39-54-216.dhcpint.csulb.edu>
2016-07-14 12:20:01 -0700

    ticks = 40 - (ex3-ex2); so far so good. need more testing to verify that it work for other cases.

--- CECS274Week2/src/ComboProgram.java
+++ CECS274Week2/src/ComboProgram.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
    int rng1 = generator.nextInt(40);
    int rng2 = generator.nextInt(40);
    int rng3 = generator.nextInt(40);
-   int ex = 10, ex2 = 13, ex3 = 1;
+   int ex = 10, ex2 = 13, ex3 = 15;
    ComboLock randomLock = new ComboLock(ex, ex2, ex3);
    
    int getDialinit = randomLock.getDialPosition();
@@ -40,7 +40,7 @@ public static void main(String[] args) {
   System.out.println("\n\n----\n get 2nd dial position " + randomLock.getDialPosition());
   
   
-  int ticks3;  // goal is to get ticks3 to equal 26, when ex is 10, ex2 is 13, and when ex3 is 27.
+  int ticks3 = 0;  // goal is to get ticks3 to equal 26, when ex is 10, ex2 is 13, and when ex3 is 27.
   // 13+26 = 39.    27-26 = 1.    13-(13) = 0.   0-(10) = 30.   30-(3) = 27.  (13)+(10)+(3) = 26. added these parentheses and got 26.
   // Anytime the dial goes pass 0 or 39, I must write the appropriate code. 13-(13) = 0. I passed 0 when 0-(10) = 30. 
   // So to go from 13 to 27, I need to turn right 26 ticks. Hmm...13-13 = 0.  So I need 13 more ticks to get to 27.  X-13 = 27. So 40-13 = 27. x = 27+13 = 40
@@ -48,9 +48,17 @@ public static void main(String[] args) {
   //  40 - 13 = 27.    40 -  13 = ex3.   
   //  2nd Dial position is at 13.  So when ex3 is greater than 2nd dial position, 27 > 13, perform fullRotation minus 2nd Dial Position and then add 2ndDial Position
   //  So 27 > 13, 13-13 = 0. ticks3 += 13. Then 40-13 = 27. ticks3 += 13. ticks3 is now 26 cuz of 13 + 13. 
-  ticks3 = (ex3 > ex2 ) ? ex3-ex2+ex2 : ex2-ex3 ;   // expression 2 or false value is currently . true condition expression 1 is ex3-ex2+ex2. so ticks3 = expression1 whenever ex3 > ex2. 
+  //ticks3 = (ex3 > ex2 ) ? ex2-ex2+ex2 : ex2-ex3 ;   // expression 2 or false value is currently . true condition expression 1 is ex3-ex2+ex2. so ticks3 = expression1 whenever ex3 > ex2. 
   // when ex3 < ex2, the expression 2 can be fullRotation - ex2. ex. 12 < 13. ex3 is 12, ex2 is 13.  so ticks3 = ex2-ex3. ticks3 should be 1 when ex2 is 13 and ex3 is 12.
   
+  // gonna use if operator instead of ternary operator since ex3 = 27 didn't give the right result for the ternary just above.
+  
+  if (ex3>ex2) {
+     ticks3 = 40 - (ex3-ex2);
+     
+  } else {
+     ticks3 = ex2 - ex3; 
+  }
   
   randomLock.turnRight(ticks3);
   System.out.println("\n\n----\n get 3rd dial position " + randomLock.getDialPosition());
