15468f24c6f3687e0b16aae816221e3bacf2dc6b
patrickly <patrickly@dhcp-39-54-216.dhcpint.csulb.edu>
2016-07-14 11:41:18 -0700

    Some more notes to figure out ticks.

--- CECS274Week2/src/ComboProgram.java
+++ CECS274Week2/src/ComboProgram.java
@@ -38,10 +38,17 @@ public static void main(String[] args) {
   
   randomLock.turnLeft(ticks2);
   System.out.println("\n\n----\n get 2nd dial position " + randomLock.getDialPosition());
-
+  
   
   int ticks3 = 26;  // goal is to get ticks3 to equal 26, when ex is 10, ex2 is 13, and when ex3 is 27.
-  // 13+26 = 39.    27-26 = 1.    13-(13) = 0.   0-(10) = 30.   30-(3) = 27.  (13)+(10)+(3) = 26. added these parentheses and got 26.  
+  // 13+26 = 39.    27-26 = 1.    13-(13) = 0.   0-(10) = 30.   30-(3) = 27.  (13)+(10)+(3) = 26. added these parentheses and got 26.
+  // Anytime the dial goes pass 0 or 39, I must write the appropriate code. 13-(13) = 0. I passed 0 when 0-(10) = 30. 
+  // So to go from 13 to 27, I need to turn right 26 ticks. Hmm...13-13 = 0.  So I need 13 more ticks to get to 27.  X-13 = 27. So 40-13 = 27. x = 27+13 = 40
+  // Guess I got to use 40 whenever the ticks passes 0. 
+  //  40 - 13 = 27.    40 -  13 = ex3.   
+  //  2nd Dial position is at 13.  So when ex3 is greater than 2nd dial position, 27 > 13, perform fullRotation minus 2nd Dial Position and then add 2ndDial Position
+  //  So 27 > 13, 13-13 = 0. ticks3 += 13. Then 40-13 = 27. ticks3 += 13. ticks3 is now 26 cuz of 13 + 13. 
+  
   randomLock.turnRight(ticks3);
   System.out.println("\n\n----\n get 3rd dial position " + randomLock.getDialPosition());
 
